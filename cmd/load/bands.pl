07e07de14eea115273999b9706b591b586e88479
djk <djk>
1998-11-13 18:43:11 +0000

    added load bands

---
+++ cmd/load/bands.pl
@@ -0,0 +1,8 @@
+#
+# load the bands  file after changing it
+#
+my $self = shift;
+return (0, $self->msg('e5')) if $self->priv < 9;
+my @out = Bands::load($self);
+@out = ($self->msg('ok')) if !@out;
+return (1, @out); 
