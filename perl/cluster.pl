5f1ef9f393b44a1e4b29b290332c386cfadcb5de
djk <djk>
1998-12-22 12:58:13 +0000

    issued 1.15

--- perl/cluster.pl
+++ perl/cluster.pl
@@ -50,7 +50,7 @@ package main;
 
 @inqueue = ();					# the main input queue, an array of hashes
 $systime = 0;					# the time now (in seconds)
-$version = "1.14";				# the version no of the software
+$version = "1.15";				# the version no of the software
 $starttime = 0;                 # the starting time of the cluster   
  
 # handle disconnections
