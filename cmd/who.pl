1f3035508fe4dacf899742db444ade501091e7de
djk <djk>
2000-06-13 23:18:06 +0000

    fixed RTT in who

--- cmd/who.pl
+++ cmd/who.pl
@@ -16,9 +16,9 @@ push @out, "  Callsign Type Started           Name                Ave RTT";
 foreach $dxchan ( sort {$a->call cmp $b->call} DXChannel::get_all ) {
     my $call = $dxchan->call();
 	my $t = cldatetime($dxchan->startt);
-	my $sort = $dxchan->is_node() ? "NODE" : "USER";
+	my $sort = $dxchan->is_node ? "NODE" : "USER";
 	my $name = $dxchan->user->name || " ";
-	my $ping = $dxchan->is_ak1a && $dxchan != $DXProt::me ? sprintf("%8.2f", $dxchan->pingave) : "";
+	my $ping = $dxchan->is_node && $dxchan != $DXProt::me ? sprintf("%8.2f", $dxchan->pingave) : "";
 	push @out, sprintf "%10s $sort $t %-18.18s $ping", $call, $name;
 }
 
