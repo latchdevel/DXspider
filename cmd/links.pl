aba7a0d5b7287daac0d3b708aa222247a2e87e86
djk <djk>
2000-06-17 23:30:05 +0000

    Added node type to links and who

--- cmd/links.pl
+++ cmd/links.pl
@@ -7,22 +7,27 @@
 #
 # 16-Jun-2000
 # $Id: links.pl
-
+#
 
 my $self = shift;
 my $dxchan;
 my @out;
 
-push @out, "  Callsign   Started                 Ave RTT";
+push @out, "  Callsign Type Started            Ave RTT";
 
 foreach $dxchan ( sort {$a->call cmp $b->call} DXChannel::get_all_ak1a ) {
 	my $call = $dxchan->call();
 	my $t = cldatetime($dxchan->startt);
+	my $sort;
 	my $name = $dxchan->user->name || " ";
 	my $ping = $dxchan->is_node && $dxchan != $DXProt::me ? sprintf("%8.2f",
 																	$dxchan->pingave) : "";
-	push @out, sprintf "%10s $t %-6.6s $ping", $call;
-
+	$sort = "DXSP" if $dxchan->is_spider;
+	$sort = "CLX " if $dxchan->is_clx;
+	$sort = "DXNT" if $dxchan->is_dxnet;
+	$sort = "AR-C" if $dxchan->is_arcluster;
+	$sort = "AK1A" if $dxchan->is_ak1a;
+	push @out, sprintf "%10s $sort $t $ping", $call;
 }
 
 return (1, @out)
--- cmd/who.pl
+++ cmd/who.pl
@@ -11,15 +11,23 @@ my $self = shift;
 my $dxchan;
 my @out;
 
-push @out, "  Callsign Type Started           Name                Ave RTT";
+push @out, "  Callsign Type      Started           Name                Ave RTT";
 
 foreach $dxchan ( sort {$a->call cmp $b->call} DXChannel::get_all ) {
     my $call = $dxchan->call();
 	my $t = cldatetime($dxchan->startt);
-	my $sort = $dxchan->is_node ? "NODE" : "USER";
+	my $type = $dxchan->is_node ? "NODE" : "USER";
+	my $sort = "    ";
+	if ($dxchan->is_node) {
+		$sort = "DXSP" if $dxchan->is_spider;
+		$sort = "CLX " if $dxchan->is_clx;
+		$sort = "DXNT" if $dxchan->is_dxnet;
+		$sort = "AR-C" if $dxchan->is_arcluster;
+		$sort = "AK1A" if $dxchan->is_ak1a;
+	}
 	my $name = $dxchan->user->name || " ";
 	my $ping = $dxchan->is_node && $dxchan != $DXProt::me ? sprintf("%8.2f", $dxchan->pingave) : "";
-	push @out, sprintf "%10s $sort $t %-18.18s $ping", $call, $name;
+	push @out, sprintf "%10s $type $sort $t %-18.18s $ping", $call, $name;
 }
 
 return (1, @out)
