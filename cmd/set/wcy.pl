6ee86589ded6e313ddb9bb748d8ba751a760692b
minima <minima>
2001-03-03 14:23:36 +0000

    add missing set/unset wcy commands

---
+++ cmd/set/wcy.pl
@@ -0,0 +1,27 @@
+#
+# set the wcy flag
+#
+# Copyright (c) 2001 - Dirk Koopman
+#
+# $Id$
+#
+
+my ($self, $line) = @_;
+my @args = split /\s+/, $line;
+my $call;
+my @out;
+
+@args = $self->call if (!@args || $self->priv < 9);
+
+foreach $call (@args) {
+  $call = uc $call;
+  my $chan = DXChannel->get($call);
+  if ($chan) {
+    $chan->wcy(1);
+    $chan->user->wantwcy(1);
+	push @out, $self->msg('wcys', $call);
+  } else {
+    push @out, $self->msg('e3', "Set WCY", $call);
+  }
+}
+return (1, @out);
---
+++ cmd/unset/wcy.pl
@@ -0,0 +1,27 @@
+#
+# unset the wcy flag
+#
+# Copyright (c) 2001 - Dirk Koopman
+#
+# $Id$
+#
+
+my ($self, $line) = @_;
+my @args = split /\s+/, $line;
+my $call;
+my @out;
+
+@args = $self->call if (!@args || $self->priv < 9);
+
+foreach $call (@args) {
+  $call = uc $call;
+  my $chan = DXChannel->get($call);
+  if ($chan) {
+    $chan->wcy(0);
+    $chan->user->wantwcy(0);
+	push @out, $self->msg('wcyu', $call);
+  } else {
+    push @out, $self->msg('e3', "Unset WCY", $call);
+  }
+}
+return (1, @out);
