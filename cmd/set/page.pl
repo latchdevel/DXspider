2dd2d417adfc86f29d4091c444e3f99906a51771
minima <minima>
2001-05-07 16:03:19 +0000

    set/page 0 switches off paging

--- cmd/set/page.pl
+++ cmd/set/page.pl
@@ -7,8 +7,7 @@
 #
 my $self = shift;
 my $l = shift;
-$l = 20 if $l == 0;
-$l = 10 if $l < 10;
+$l = 10 if $l < 10 && $l > 0;
 $self->pagelth($l);
 $self->user->pagelth($l);
 return (1, $self->msg('pagelth', $l));
