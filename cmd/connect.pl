015e51891a2052454449c99fde521a36efdad9e2
minima <minima>
2001-05-15 12:41:50 +0000

    set/lockout now prevents any outgoing connect taking place

--- cmd/connect.pl
+++ cmd/connect.pl
@@ -11,6 +11,9 @@ return (1, $self->msg('already', $call)) if DXChannel->get($call);
 return (1, $self->msg('outconn', $call)) if grep {$_->{call} eq $call} @main::outstanding_connects;
 return (1, $self->msg('conscript', $lccall)) unless -e "$main::root/connect/$lccall";
 
+my $user = DXUser->get($call);
+return (1, $self->msg('lockout', $call)) if $user->lockout;
+
 my @out;
 push @out, $self->msg('constart', $call);
 ExtMsg::start_connect($call, "$main::root/connect/$lccall");
