7aa6d9d72bd4dca8514f2a308dfbd6d91d0e7330
minima <minima>
2001-06-06 14:39:10 +0000

    add show/userconfig command

---
+++ cmd/show/userconfig.pl
@@ -0,0 +1,22 @@
+#
+# show who all the users are connected to
+#
+# Copyright (c) 2001 Dirk Koopman G1TLH
+#
+# $Id$
+#
+
+my ($self, $line) = @_;
+my @list = map { uc } split /\s+/, $line;           # list of callsigns of nodes
+my @out;
+my @users = sort {$a->call cmp $b->call} values %Route::User::list;
+
+foreach my $uref (@users) {
+	my $ucall = $uref->call;
+	next if @list && !grep $ucall =~ m|$_|, @list;
+	my $call = $uref->user_call;
+	my $l = join ',', (map {my $ref = Route::Node::get($_); $ref ? ($ref->user_call) : ("$_?")} sort @{$uref->parent});
+	push @out, "$call->$l";
+}
+
+return (1, @out);
