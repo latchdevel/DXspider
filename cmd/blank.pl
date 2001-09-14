46ac39b6f5ae340d6ce5ab930f712e1f3a367b57
minima <minima>
2001-09-14 23:03:57 +0000

    enhance the blank command

--- cmd/blank.pl
+++ cmd/blank.pl
@@ -7,8 +7,19 @@
 #
 
 my ($self, $line) = @_;
-my ($lines) = $line =~ /^\s*(\d+)/;
-$lines ||= 1;
+my $lines = 1;
+my $data = ' ';
+my @f = split /\s+/, $line;
+if (@f && $f[0] !~ /^\d+$/) {
+	$data = shift @f;
+	$data = $data x int(($self->width-1) / length($data));
+	$data .= substr $data, 0, int(($self->width-1) % length($data))
+}
+if (@f && $f[0] =~ /^\d+$/) {
+	$lines = shift @f;
+	$lines = 9 if $lines < 9;
+	$lines = 1 if $lines < 1;
+}
 my @out;
-push @out, ' ' for (1..$lines);
+push @out, $data for (1..$lines);
 return (1, @out);
