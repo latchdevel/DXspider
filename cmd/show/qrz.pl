05e7fa367c898dc3d1a168d6606c56b8050569ca
minima <minima>
2002-01-08 13:18:56 +0000

    point to the new server

--- cmd/show/qrz.pl
+++ cmd/show/qrz.pl
@@ -13,10 +13,10 @@ my @out;
 
 return (1, $self->msg('e24')) unless $Internet::allow;
 return (1, "SHOW/QRZ <callsign>, e.g. SH/QRZ g1tlh") unless @list;
-my $target = $Internet::http_proxy || 'qrz.com';
+my $target = $Internet::http_proxy || 'u2.qrz.com';
 my $port = $Internet::http_proxy_port || 80;
 my $url = '';
-$url = 'http://qrz.com' if $Internet::http_proxy; 
+$url = 'http://u2.qrz.com' if $Internet::http_proxy; 
 
 use Net::Telnet;
 
@@ -33,7 +33,7 @@ foreach $l (@list) {
 		push @out, $self->msg('e18', 'QRZ.com');
 	} else {
 		my $s = "GET $url/dxcluster.cgi?callsign=$l\&uid=$Internet::qrz_uid\&pw=$Internet::qrz_pw HTTP/1.0\n\n";
-#		print $s;
+		dbg($s) if isdbg('qrz');
 		$t->print($s);
 		Log('call', "$call: show/qrz \U$l");
 		my $state = "blank";
