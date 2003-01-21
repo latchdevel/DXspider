1980462a73fe3a14acc44c9a75982d8b15e9390c
minima <minima>
2003-01-21 10:54:20 +0000

    add Agent

---
+++ perl/Log/Agent.pm
@@ -0,0 +1,28 @@
+#
+# Dummy Log Agent
+#
+# This is just for the benefit of Storable on 5.8.0
+#
+# Copyright (c) Dirk Koopman
+#
+# $Id$
+#
+
+package Log::Agent;
+
+use DXDebug;
+
+$VERSION = 0.3;
+@ISA = qw(Exporter);
+@EXPORT = qw(logcroak logcarp);
+
+sub logcroak
+{
+	DXDebug::croak(@_);
+}
+
+sub logcarp
+{
+	DXDebug::carp(@_);
+}
+1;
