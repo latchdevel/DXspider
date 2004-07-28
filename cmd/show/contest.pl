17e88d739dbfcab7c690a071f9f5470a354f2011
minima <minima>
2004-07-28 14:26:07 +0000

    fix sh/contest so that it's urls are in Internet.pm

--- cmd/show/contest.pl
+++ cmd/show/contest.pl
@@ -9,7 +9,7 @@
 
 my ($self, $line) = @_;
 
-return (1, "usage: sh/contest  <year_and_month>, e g sh/contest 2002sep ") unless $line;
+#return (1, "usage: sh/contest  <year_and_month>, e g sh/contest 2002sep ") unless $line;
 
 my @out;
 
@@ -20,18 +20,24 @@ my $mon;;
 $line = lc $line;
 my ($m,$y) = $line =~ /^([a-z]{3})\w*\s*(\d+)/;
 ($y,$m) = $line =~ /^(\d+)\s*([a-z]{3})/ unless $y && $m;
+unless ($y && $m) {
+	($m,$y) = (gmtime)[4,5];
+	$m = qw(jan feb mar apr may jun jul aug sep oct nov dec)[$m];
+	$y += 1900;
+}
 $y += 2000 if $y <= 50;
 $y += 1900 if $y > 50 && $y <= 99;
 $m = 'oct' if $m eq 'okt';
 $m = 'may' if $m eq 'mai' || $m eq 'maj';
 $mon = "$y$m";
 
-dbg($mon);
+dbg($mon) if isdbg('contest');
 
 my $filename = "c" . $mon . ".txt";
-my $host = 'www.sk3bg.se';
+my $host = $Internet::contest_host || 'www.sk3bg.se';
 my $port = 80;
-my $url = "http://www.sk3bg.se/contest/text/$filename";
+my $url = $Internet::contest_url || "http://www.sk3bg.se/contest/text";
+$url .= "/$filename";
 
 my $t = new Net::Telnet (Telnetmode => 0);
 eval {
@@ -41,7 +47,7 @@ eval {
 if (!$t || $@) {
     push @out, $self->msg('e18','sk3bg.se');
 } else {
-    my $s = "GET http://www.sk3bg.se/contest/text/$filename";
+    my $s = "GET $url";
     $t->print($s);
     my $notfound = $t->getline(Timeout => 10);
     if ($notfound =~ /404 Object Not Found/) {
