b26c5204bc58c12fc4d61725ba4f1cfc1a38be45
minima <minima>
2004-12-22 00:06:49 +0000

    Changed links command to Charlie K1XX's version. Added sh/grourps command made other changes to wpxloc.raw

--- cmd/links.pl
+++ cmd/links.pl
@@ -4,6 +4,7 @@
 #
 # Created by Iain Philipps G0RDI, based entirely on
 # who.pl, which is Copyright (c) 1999 Dirk Koopman G1TLH
+# and subsequently plagerized by K1XX.
 #
 # 16-Jun-2000
 # $Id: links.pl
@@ -12,22 +13,27 @@
 my $self = shift;
 my $dxchan;
 my @out;
+my $nowt = time;
 
-push @out, "  Callsign Type Started            Ave RTT";
+push @out, "                                      Ave   Obs   Ping   Sec Since";
+push @out, "  Callsign Type Started               RTT  count  Int.   Last Ping";
 
 foreach $dxchan ( sort {$a->call cmp $b->call} DXChannel::get_all_nodes ) {
 	my $call = $dxchan->call();
+	next if $dxchan == $main::me;
 	my $t = cldatetime($dxchan->startt);
 	my $sort;
 	my $name = $dxchan->user->name || " ";
-	my $ping = $dxchan->is_node && $dxchan != $main::me ? sprintf("%8.2f",
-																	$dxchan->pingave) : "";
+	my $obscount = $dxchan->nopings;
+	my $lastt = $nowt - ($dxchan->lastping);
+	my $pingint = $dxchan->pingint;
+	my $ping = $dxchan->is_node && $dxchan != $main::me ? sprintf("%8.2f",$dxchan->pingave) : "";
 	$sort = "DXSP" if $dxchan->is_spider;
 	$sort = "CLX " if $dxchan->is_clx;
 	$sort = "DXNT" if $dxchan->is_dxnet;
 	$sort = "AR-C" if $dxchan->is_arcluster;
 	$sort = "AK1A" if $dxchan->is_ak1a;
-	push @out, sprintf "%10s $sort $t $ping", $call;
+	push @out, sprintf "%10s $sort $t$ping    $obscount   %5d       %5d", $call, $pingint, $lastt;
 }
 
 return (1, @out)
