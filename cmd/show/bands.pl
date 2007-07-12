ba5625a459d1418cdc05591ae556df58d509ac9d
Dirk Koopman <djk@tobit.co.uk>
2007-07-12 17:20:49 +0100

    Add help for sh/band and improve sh/band command
    Allow sh/bands to have arguments so people can query
    a band or region.

--- cmd/show/bands.pl
+++ cmd/show/bands.pl
@@ -6,34 +6,43 @@
 #
 #
 
-#$DB::single = 1;
+$DB::single = 1;
 
 my ($self, $line) = @_;
 my @f = split /\s+/, $line;
-my @bands;
+my @bands = grep {Bands::get($_)?$_:()} @f;
+my @regs = grep {Bands::get_region($_)?$_:()} @f;
 my $band;
 my @out;
 my $i;
 
-if (!$line) {
-	@bands = sort { Bands::get($a)->band->[0] <=> Bands::get($b)->band->[0] } Bands::get_keys();
+unless (@f) {
+	@bands = Bands::get_keys();
+	@regs =  Bands::get_region_keys();
+}
+if (@bands) {
+	@bands = sort { Bands::get($a)->band->[0] <=> Bands::get($b)->band->[0] } @bands;
 	push @out, "Bands Available:-";
-	foreach $band (@bands) {
-		my $ref = Bands::get($band)->band;
-		my $s = sprintf "%10s: ", $band;
-		for ($i = 0; $i < $#{$ref}; $i += 2) {
+	foreach my $name (@bands) {
+		my $band = Bands::get($name);
+		my $ref = $band->band;
+		my $s = sprintf "%10s: ", $name;
+		for ($i = 0; $i < @$ref; $i += 2) {
 			my $from = $ref->[$i];
 			my $to = $ref->[$i+1];
 			$s .= ", " if $i;
 			$s .= "$from -> $to";
 		}
 		push @out, $s;
-	} 
+	}
+}
+
+if (@regs) {
 	push @out, "Regions Available:-";
-	@bands = Bands::get_region_keys();
-	foreach $band (@bands) {
-		my $ref = Bands::get_region($band);
-		my $s = sprintf("%10s: ", $band ) . join(' ', @{$ref}); 
+	@regs = sort @regs;
+	foreach my $region (@regs) {
+		my $ref = Bands::get_region($region);
+		my $s = sprintf("%10s: ", $region ) . join(' ', @{$ref});
 		push @out, $s;
 	}
 }
