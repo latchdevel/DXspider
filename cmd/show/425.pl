01c7ebc6ec371bc6e075a599dad1d5f3fd1b776a
Dirk Koopman <djk@tobit.co.uk>
2008-11-17 00:09:29 +0000

    fix sh/425 to point to new server
    unfortunately this is http/1.1 only which causes problems with
    detecting the EOF ('cos there ain't one). So it is slower than it was.
    
    Add CTY-1814 prefixes 'Kingman Reef'

--- cmd/show/425.pl
+++ cmd/show/425.pl
@@ -14,9 +14,9 @@ my @out;
 return (1, $self->msg('e24')) unless $Internet::allow;
 return (1, "SHOW/425 <callsign>\nSHOW/425 CAL\nSHOW/425 BULL <bulletin number>\n e.g. SH/425 IQ5BL, SH/425 CAL, SH/425 BUL 779\n") unless @list;
 
-my $target = "www.ari.it";
+my $target = "www.ariscandicci.it";
 my $port = 80;
-my $url = "http://www.ari.it";
+my $url = "http://www.ariscandicci.it";
 
 use Net::Telnet;
 my $t = new Net::Telnet;
@@ -38,19 +38,23 @@ if (!$t || $@) {
         else {
                 $op="op=search&query=".$list[0];
         }
-	my $s = "GET $url/hf/dx-news/spider.php?$op HTTP/1.0\n" 
-        ."User-Agent:DxSpider;$main::version;$main::build;$^O;$main::mycall;$call;$list[0]\n\n";
+	my $s = "GET /425dxn/spider.php?$op HTTP/1.1\n"
+        ."User-Agent:DxSpider;$main::version;$main::build;$^O;$main::mycall;$call;$list[0]\nHost: $target\n\n";
         dbg($s) if isdbg('425');
         $t->print($s);
         Log('call', "$call: show/425 \U$op");
         my $state = "blank";
         my $count = 1;
-        while (my $result = eval { $t->getline(Timeout => 30) } || $@) {
+		my @timeout = qw(Timeout 30);
+        while (my $result = eval { $t->getline(@timeout) } || $@) {
                 dbg($result) if isdbg('425') && $result;
+                dbg($@) if isdbg('425') && $@;
                 ++$count;
                 if ($count > 9) {
+						last if $@;
                         push @out, $result;
                 }
+				@timeout = qw(Timeout 2);
         }
         $t->close;
         push @out, $self->msg('e3', 'Search(ARI.org)', uc $op) unless @out;
