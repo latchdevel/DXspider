2a187a44dc84414399786d1251ef4316c2d0009b
Dirk Koopman <djk@tobit.co.uk>
2010-10-05 00:38:41 +0100

    up issue version...

--- perl/Version.pm
+++ perl/Version.pm
@@ -11,7 +11,7 @@ use vars qw($version $subversion $build $gitversion);
 
 $version = '1.55';
 $subversion = '0';
-$build = '71';
-$gitversion = 'cd5b993';
+$build = '73';
+$gitversion = '1d6f1b1';
 
 1;
