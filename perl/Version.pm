059fb24925845865ca791ba53bb540444931f280
Dirk Koopman <djk@tobit.co.uk>
2010-10-15 20:42:58 +0100

    add cty-2007

--- perl/Version.pm
+++ perl/Version.pm
@@ -11,7 +11,7 @@ use vars qw($version $subversion $build $gitversion);
 
 $version = '1.55';
 $subversion = '0';
-$build = '73';
-$gitversion = '1d6f1b1';
+$build = '74';
+$gitversion = '2a187a4';
 
 1;
