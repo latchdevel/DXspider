4ab258ae110cf695ba16069567f9e161cfa23e7a
Dirk Koopman <djk@tobit.co.uk>
2011-10-25 15:23:41 +0100

    add CTY2113 prefixes

--- perl/Version.pm
+++ perl/Version.pm
@@ -11,7 +11,7 @@ use vars qw($version $subversion $build $gitversion);
 
 $version = '1.55';
 $subversion = '0';
-$build = '93';
-$gitversion = '258f3e1';
+$build = '94';
+$gitversion = '6262a02';
 
 1;
