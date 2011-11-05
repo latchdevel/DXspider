192c703baf1c01012bdf210ab2e1fca686be9c9c
Dirk Koopman <djk@tobit.co.uk>
2011-11-05 17:20:43 +0000

    added cty2116 changes with fixed version

--- perl/Version.pm
+++ perl/Version.pm
@@ -11,7 +11,7 @@ use vars qw($version $subversion $build $gitversion);
 
 $version = '1.55';
 $subversion = '0';
-$build = '95';
-$gitversion = '9bbfe9a';
+$build = '97';
+$gitversion = '8420176';
 
 1;
