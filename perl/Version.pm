4e42369e6f34642784560dc41e54bf5cb029757f
Dirk Koopman <djk@tobit.co.uk>
2012-03-09 23:02:43 +0000

    fix UNIVERSAL typo in DXProt for pc34

--- perl/Version.pm
+++ perl/Version.pm
@@ -11,7 +11,7 @@ use vars qw($version $subversion $build $gitversion);
 
 $version = '1.56';
 $subversion = '0';
-$build = '29';
-$gitversion = '8423d02';
+$build = '30';
+$gitversion = '899ab26';
 
 1;
