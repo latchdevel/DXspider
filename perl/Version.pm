a55492232d670247a2c1fb3406a129bc04847e8b
Dirk Koopman <djk@tobit.co.uk>
2013-09-06 14:26:47 +0100

    update Changes

--- perl/Version.pm
+++ perl/Version.pm
@@ -11,7 +11,7 @@ use vars qw($version $subversion $build $gitversion);
 
 $version = '1.55';
 $subversion = '0';
-$build = '123';
-$gitversion = 'f67292b';
+$build = '124';
+$gitversion = 'c675748';
 
 1;
