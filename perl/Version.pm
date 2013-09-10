2afa578fe5c80c047c67427a3002eea0392727d0
Dirk Koopman <djk@tobit.co.uk>
2013-09-11 00:36:45 +0100

    get 302 working in AsyncMsg, basic get/keps

--- perl/Version.pm
+++ perl/Version.pm
@@ -11,7 +11,7 @@ use vars qw($version $subversion $build $gitversion);
 
 $version = '1.55';
 $subversion = '0';
-$build = '133';
-$gitversion = 'e941823';
+$build = '134';
+$gitversion = 'b099b4a';
 
 1;
