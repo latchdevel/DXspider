981c165cd4ab26cc099879a939d78dcb1051cfe8
Dirk Koopman <djk@tobit.co.uk>
2014-06-19 23:18:42 +0100

    put back Version.pm, auto update to difficult

---
+++ perl/Version.pm
@@ -0,0 +1,16 @@
+#
+# Version information for DXSpider
+#
+# DO NOT ALTER THIS FILE. It is generated automatically
+# and will be overwritten
+#
+
+package main;
+
+use vars qw($version $build $gitversion);
+
+$version = '1.57';
+$build = '33';
+$gitversion = '4b94818';
+
+1;
