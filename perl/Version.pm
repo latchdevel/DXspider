6b600a49733f83a8ae38383a33d38adff71f2b2f
Dirk Koopman <djk@tobit.co.uk>
2014-08-30 16:51:32 +0100

    add CTY-2409 prefixes

--- perl/Version.pm
+++ perl/Version.pm
@@ -11,7 +11,7 @@ use vars qw($version $subversion $build $gitversion);
 
 $version = '1.55';
 $subversion = '0';
-$build = '146';
-$gitversion = '66d98a2';
+$build = '157';
+$gitversion = '810e1ba';
 
 1;
