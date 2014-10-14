7a395060c59baffc5d2a57c2dbcbed89aa85dd1e
Dirk Koopman <djk@tobit.co.uk>
2014-10-14 11:45:43 +0100

    upissue build

--- perl/Version.pm
+++ perl/Version.pm
@@ -11,7 +11,7 @@ use vars qw($version $subversion $build $gitversion);
 
 $version = '1.55';
 $subversion = '0';
-$build = '158';
-$gitversion = '6b600a4';
+$build = '160';
+$gitversion = '56478c6';
 
 1;
