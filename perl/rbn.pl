812bf47870fa01eccce9d08c8a053659b8ddc6a1
Dirk Koopman <djk@tobit.co.uk>
2017-01-03 11:11:51 +0000

    fix stats display on rbn.pl
    Make sure that a stats display happens every minute.
    Add the rbn arg to allow the display of (de-duped) rbn spots.

--- perl/rbn.pl
+++ perl/rbn.pl
@@ -12,7 +12,7 @@ use Math::Round qw(nearest);
 
 my $host = 'telnet.reversebeacon.net';
 my $port = 7000;
-my $mycall = shift or die "usage:rbn.pl <callsign> [debug] [stats] [cw] [rtty] [psk] [beacon] [<time between repeat spots in minutes>]\n"; 
+my $mycall = shift or die "usage:rbn.pl <callsign> [debug] [stats] [cw] [rtty] [psk] [beacon] [<time between repeat spots in minutes>] [rbn]\n"; 
 
 my $minspottime = 30*60;		# minimum length of time between successive spots
 my $showstats;					# show RBN and Spot stats
@@ -24,13 +24,15 @@ my $wantcw = 1;
 my $wantrtty = 1;
 my $wantpsk = 1;
 my $wantbeacon = 1;
-my $override; 
+my $override;
+my $showrbn;
 	
 while (@ARGV) {
 	my $arg = shift;
 
 	++$dbg if $arg =~ /^deb/i;
 	++$showstats if $arg =~ /^stat/i;
+	++$showrbn if $arg =~ /^rbn/i;
 	$minspottime = $arg * 60 if $arg =~ /^\d+$/;
 	if (!$override && $arg =~ /^cw|rtty|psk|beacon$/i) {
 		$override = 1;
@@ -69,8 +71,53 @@ while (<$sock>) {
 	chomp;
 	my $tim = time;
 	
+	# parse line
+	my (undef, undef, $origin, $qrg, $call, $mode, $s, $m, $spd, $u, $sort, $t) = split /[:\s]+/;
+	if ($t) {
+
+		# We have an RBN data line, dedupe it very simply on time, ignore QRG completely.
+		# This works because the skimmers are NTP controlled (or should be) and will receive
+		# the spot at the same time (velocity factor of the atmosphere taken into account :-)
+		my $p = "$t|$call";
+		++$noraw;
+		next if $d{$p};
+
+		# new RBN input
+		$d{$p} = $tim;
+		++$norbn;
+		$qrg = sprintf('%.1f', nearest(.1, $qrg));     # to nearest 100Hz (to catch the odd multiple decpl QRG [eg '7002.07']).
+		say join(',', "RBN", $origin, $qrg, $call, $mode, $s, $m, $spd, $u, $sort, $t) if $dbg || $showrbn;
+
+		# Determine whether to "SPOT" it based on whether we have not seen it before (near this QRG) or,
+		# if we have, has it been a "while" since the last time we spotted it? If it has been spotted
+		# before then "RESPOT" it.
+		my $nqrg = nearest(1, $qrg);  # normalised to nearest Khz
+		my $sp = "$call|$nqrg";		  # hopefully the skimmers will be calibrated at least this well! 
+		my $ts = $spot{$sp};
+		
+		if (!$ts || $tim - $ts >= $minspottime) {
+			if ($wantbeacon && $sort =~ /^BEA/) {
+				;
+			} else {
+				# Haven't used a perl 'goto' like this ever!
+				# Clearly I need to use an event driven framework :-) 
+				goto periodic if !$wantcw  && $mode =~ /^CW/;
+				goto periodic if !$wantrtty && $mode =~ /^RTTY/;
+				goto periodic if !$wantpsk && $mode =~ /^PSK/;
+			}
+
+			++$nospot;
+			my $tag = $ts ? "RESPOT" : "SPOT";
+			say join(',', $tag, $origin, $qrg, $call, $mode, $s, $m, $spd, $u, $sort, $t);
+			$spot{$sp} = $tim;
+		}
+	} else {
+		say "data,$_" if $dbg;
+	}
+
+ periodic:
 	# periodic clearing out of the two caches
-	if ($tim % 60 == 0 && $last != $tim) {
+	if (($tim % 60 == 0 && $tim > $last) || ($last && $tim >= $last + 60)) {
 		my $count = 0;
 		my $removed = 0;
 		
@@ -97,49 +144,7 @@ while (<$sock>) {
 		say join(',', "STAT", $noraw, $norbn, $nospot) if $showstats;
 		$noraw = $norbn = $nospot = 0;
 
-		$last = $tim;
-	}
-
-	# parse line
-	my (undef, undef, $origin, $qrg, $call, $mode, $s, $m, $spd, $u, $sort, $t) = split /[:\s]+/;
-	if ($t) {
-
-		# We have an RBN data line, dedupe it very simply on time, ignore QRG completely.
-		# This works because the skimmers are NTP controlled (or should be) and will receive
-		# the spot at the same time (velocity factor of the atmosphere taken into account :-)
-		my $p = "$t|$call";
-		++$noraw;
-		next if $d{$p};
-
-		# new RBN input
-		$d{$p} = $tim;
-		++$norbn;
-		$qrg = sprintf('%.1f', nearest(.1, $qrg));     # to nearest 100Hz (to catch the odd multiple decpl QRG [eg '7002.07']).
-		say join(',', "RBN", $origin, $qrg, $call, $mode, $s, $m, $spd, $u, $sort, $t) if $dbg;
-
-		# Determine whether to "SPOT" it based on whether we have not seen it before (near this QRG) or,
-		# if we have, has it been a "while" since the last time we spotted it? If it has been spotted
-		# before then "RESPOT" it.
-		my $nqrg = nearest(1, $qrg);  # normalised to nearest Khz
-		my $sp = "$call|$nqrg";		  # hopefully the skimmers will be calibrated at least this well! 
-		my $ts = $spot{$sp};
-		
-		if (!$ts || $tim - $ts >= $minspottime) {
-			if ($wantbeacon && $sort =~ /^BEA/) {
-				;
-			} else {
-				next if !$wantcw  && $mode =~ /^CW/;
-				next if !$wantrtty && $mode =~ /^RTTY/;
-				next if !$wantpsk && $mode =~ /^PSK/;
-			}
-
-			++$nospot;
-			my $tag = $ts ? "RESPOT" : "SPOT";
-			say join(',', $tag, $origin, $qrg, $call, $mode, $s, $m, $spd, $u, $sort, $t);
-			$spot{$sp} = $tim;
-		}
-	} else {
-		say "data,$_" if $dbg;
+		$last = int($tim / 60) * 60;
 	}
 }
 
