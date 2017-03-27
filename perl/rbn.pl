ee6feb1687745805069c8f539a9ddc9998022667
Dirk Koopman <djk@tobit.co.uk>
2017-03-27 12:37:02 +0100

    fix rbn.pl

--- perl/rbn.pl
+++ perl/rbn.pl
@@ -74,8 +74,8 @@ for ($attempts = 1; $attempts <= 5; ++$attempts) {
 
 die "ADMIN,Cannot connect to $host:$port after 5 attempts $!" unless $sock;
 say "ADMIN,connected" if $dbg;
-#print $sock "$mycall\r\n";
-#say "ADMIN,call $mycall sent" if $dbg;
+print $sock "$mycall\r\n";
+say "ADMIN,call $mycall sent" if $dbg;
 
 my %d;
 my %spot;
