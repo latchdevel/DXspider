fabf556875ff78823220ef70051640b2639f731b
Dirk Koopman <djk@tobit.co.uk>
2019-02-16 17:00:59 +0000

    revert export_users to in process action
    This potentially does deletes which is a problem (at least for now) on spawned commands

--- cmd/export_users.pl
+++ cmd/export_users.pl
@@ -8,9 +8,14 @@ my $line = shift;;
 return (1, $self->msg('e5')) unless $self->priv >= 9;
 
 my ($fn, $flag) = split /\s+/, $line;
+$fn ||= 'user_asc';
+unless ($fn && $fn eq 'user_asc') {
+	$fn =~ s|[/\.]||g;
+	$fn = "/tmp/$fn";
+}
 my $strip = $flag eq 'strip';
 
-my @out = $self->spawn_cmd("export_users", \&DXUser::export, args => [$fn, $strip]);
+my @out = DXUser::export($fn, $strip);
 
 return (1, @out);
 
