c3c40fa3708eb08ec5e81dbbff35deec6e2c44e4
Dirk Koopman <djk@tobit.co.uk>
2019-09-10 17:02:53 +0100

    improve DXQsl handling and fix crashes?

---
+++ cmd/load/dxqsl.pl
@@ -0,0 +1,8 @@
+#
+# load the QSL file after changing it
+#
+my $self = shift;
+return (1, $self->msg('e5')) if $self->priv < 9;
+QSL::finish();
+my $r = QSL::init(1);
+return (1, $r ? $self->msg('ok') : $self->msg('e2', "$!"));
