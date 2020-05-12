3b932bf2afb86d5373c68ada44bb8f5eb642514f
Dirk Koopman <djk@tobit.co.uk>
2020-05-12 23:26:37 +0100

    Restore Script.pm to master's version

--- perl/Script.pm
+++ perl/Script.pm
@@ -22,7 +22,7 @@ my $base = "$main::root/scripts";
 sub clean
 {
 	my $s = shift;
-	$s =~ s/([-\w\d_]+)/$1/g;
+	$s =~ s/[^-\w\.]//g;
 	return $s;
 }
 
@@ -118,26 +118,5 @@ sub lines
 sub erase
 {
 	my $self = shift;
-	my $call = clean($self->{call});
-
-	my $fn;
-	my $try;
-
-	$try = "$base/" . clean(lc $self->call);
-	if (-w $try) {
-		$fn = $try;
-	} else {
-		$try = "$base/" . clean(uc $self->call);
-		if (-w $try) {
-			$fn = $try;
-		}
-	}
-
-	if ($fn && -w $fn) {
-		unless (unlink $fn) {
-			return ($self->msg('m22'. $call)); 
-		}
-		return ($self->msg('m20', $call));
-	}
-	return ($self->msg('e3', "unset/startup", $call));
+	unlink $self->{fn};
 }
