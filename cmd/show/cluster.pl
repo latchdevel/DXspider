8fa1ac1083c4a4d1b2916933ca49d00d2fad2d8b
Dirk Koopman <djk@tobit.co.uk>
2020-07-07 18:56:52 +0100

    fix (bandaid for now) sh/cl local counts

--- cmd/show/cluster.pl
+++ cmd/show/cluster.pl
@@ -6,4 +6,7 @@ my $self = shift;
 
 my ($nodes, $tot, $users, $maxlocalusers, $maxusers, $uptime, $localnodes) = Route::cluster();
 
+$localnodes = $main::routeroot->nodes;
+$users = $main::routeroot->users;
+
 return (1, $self->msg('cluster', $localnodes, $nodes, $users, $tot, $maxlocalusers, $maxusers, $uptime));
